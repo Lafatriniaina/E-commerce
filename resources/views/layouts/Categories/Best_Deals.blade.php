c34bfda129d375ff8901a11407306d4a3a026ba7
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-11-15 21:58:54 +0300

    Model the user view

---
+++ resources/views/layouts/Categories/Best_Deals.blade.php
@@ -0,0 +1,95 @@
+<div class="mb-2 mt-5">
+    <div class="d-flex justify-content-between mb-3">
+      <h3>Nos meilleurs offres</h3>
+        <button class="other-products fw-bold d-none d-md-block rounded border-0 px-5" href="#!">
+            Voir plus
+            <svg class="svg-inline--fa fa-chevron-right fs--1 ms-1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg="">
+                <path fill="currentColor" d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"></path>
+            </svg>  
+        </button>
+    </div>
+
+    <div class="categories-lists container">
+        <div class="row overflow-x-auto" style="height: 325px;">
+            <div class="col-lg-2 col-md-4 col-sm-2 m-2 border border-1 rounded-3 border-grey">
+                <img class="h-50 w-100 mt-2 rounded" src="{{ asset('images/vetements/ensemble_debardeur_72_80.jpg') }}" alt="First image">
+                <p class="mt-3">Categories title</p>
+                <div class="price text-success font-weight-bold">12.000 Ar</div>
+                <div class="mt-2 d-flex justify-content-around">
+                    <div class="Adding-to-pannier">
+                        <button class="btn btn-primary">Ajouter au pannier</button>
+                    </div>
+                    <div class="heart-love d-flex align-items-center justify-content-center rounded p-3 border-lg border-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="feather feather-heart text-dark text-700" style="height:30px;width:30px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>                         
+                    </div>                     
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-sm-2 m-2 border border-1 rounded-3 border-grey">
+                <img class="h-50 w-100 mt-2 rounded" src="{{ asset('images/vetements/ensemble_debardeur_72_80.jpg') }}" alt="First image">
+                <p class="mt-3">Categories title</p>
+                <div class="price text-success font-weight-bold">12.000 Ar</div>
+                <div class="mt-2 d-flex justify-content-around">
+                    <div class="Adding-to-pannier">
+                        <button class="btn btn-primary">Ajouter au pannier</button>
+                    </div>
+                    <div class="heart-love d-flex align-items-center justify-content-center rounded p-3 border-lg border-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="feather feather-heart text-dark text-700" style="height:30px;width:30px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>                         
+                    </div>                     
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-sm-2 m-2 border border-1 rounded-3 border-grey">
+                <img class="h-50 w-100 mt-2 rounded" src="{{ asset('images/vetements/ensemble_debardeur_72_80.jpg') }}" alt="First image">
+                <p class="mt-3">Categories title</p>
+                <div class="price text-success font-weight-bold">12.000 Ar</div>
+                <div class="mt-2 d-flex justify-content-around">
+                    <div class="Adding-to-pannier">
+                        <button class="btn btn-primary">Ajouter au pannier</button>
+                    </div>
+                    <div class="heart-love d-flex align-items-center justify-content-center rounded p-3 border-lg border-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="feather feather-heart text-dark text-700" style="height:30px;width:30px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>                         
+                    </div>                     
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-sm-2 m-2 border border-1 rounded-3 border-grey">
+                <img class="h-50 w-100 mt-2 rounded" src="{{ asset('images/vetements/ensemble_debardeur_72_80.jpg') }}" alt="First image">
+                <p class="mt-3">Categories title</p>
+                <div class="price text-success font-weight-bold">12.000 Ar</div>
+                <div class="mt-2 d-flex justify-content-around">
+                    <div class="Adding-to-pannier">
+                        <button class="btn btn-primary">Ajouter au pannier</button>
+                    </div>
+                    <div class="heart-love d-flex align-items-center justify-content-center rounded p-3 border-lg border-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="feather feather-heart text-dark text-700" style="height:30px;width:30px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>                         
+                    </div>                     
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-sm-2 m-2 border border-1 rounded-3 border-grey">
+                <img class="h-50 w-100 mt-2 rounded" src="{{ asset('images/vetements/ensemble_debardeur_72_80.jpg') }}" alt="First image">
+                <p class="mt-3">Categories title</p>
+                <div class="price text-success font-weight-bold">12.000 Ar</div>
+                <div class="mt-2 d-flex justify-content-around">
+                    <div class="Adding-to-pannier">
+                        <button class="btn btn-primary">Ajouter au pannier</button>
+                    </div>
+                    <div class="heart-love d-flex align-items-center justify-content-center rounded p-3 border-lg border-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="feather feather-heart text-dark text-700" style="height:30px;width:30px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>                         
+                    </div>                     
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
