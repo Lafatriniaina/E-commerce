f5a9855e81ddf53ff628367291faedd75927532f
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-11-16 22:18:22 +0300

    Add additional route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.home');
-})->name('home');
-
+Route::get('/', [ProductsController::class, 'index'])->name('home');
 
 Route::prefix('/user')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
@@ -27,6 +24,10 @@ Route::prefix('/user')->group(function () {
 
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('auth.login');
+
+    Route::get('/description/{name}/', function () {
+        return view('layouts.Description');
+    })->name('description');
 });
 
 require __DIR__.'/auth.php';
