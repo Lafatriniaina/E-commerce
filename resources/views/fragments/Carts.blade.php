89e28e140cc5627c1733b5dc2ac6a2a6393e7e1a
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-11-16 22:17:35 +0300

    Carts

---
+++ resources/views/fragments/Carts.blade.php
@@ -0,0 +1,52 @@
+<div class="cart border-right-0 bg-default position-absolute top-0" style="z-index: 1;">
+    <div class="title d-flex justify-content-between mx-5 mb-2">
+        <h3>Carts</h3>
+        <button class="close-cart border-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 40px;">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+    </div><hr>
+    <div class="d-flex justify-content-around my-3 align-items-center me-4">
+        <div class="image d-flex align-items-center">
+            <img src="{{ asset('images/electronics/2.png') }}" alt="images" style="width: 100px; height: 100px;">
+            <div class="content">
+                <p class="categories">Pantalon cuire</p>
+                <p class="price">100.000 Ar</p>
+            </div>
+        </div>
+        <div class="form-outline d-flex" style="width: 90px; height: 40px;">
+            <button class="decrement btn btn-primary mx-1">-</button>
+            <input type="number" class="quantity form-control" style="width: 40px; height: 40px;" />
+            <button class="increment btn btn-primary mx-1">+</button>
+        </div>
+    </div>
+    <div class="d-flex justify-content-around my-3 align-items-center me-4">
+        <div class="image d-flex align-items-center">
+            <img src="{{ asset('images/electronics/2.png') }}" alt="images" style="width: 100px; height: 100px;">
+            <div class="content">
+                <p class="categories">Pantalon cuire</p>
+                <p class="price">100.000 Ar</p>
+            </div>
+        </div>
+        <div class="form-outline d-flex" style="width: 90px; height: 40px;">
+            <button class="decrement btn btn-primary mx-1">-</button>
+            <input type="number" class="quantity form-control" style="width: 40px; height: 40px;" />
+            <button class="increment btn btn-primary mx-1">+</button>
+        </div>
+    </div>
+    <div class="d-flex justify-content-around my-3 align-items-center me-4">
+        <div class="image d-flex align-items-center">
+            <img src="{{ asset('images/electronics/2.png') }}" alt="images" style="width: 100px; height: 100px;">
+            <div class="content">
+                <p class="categories">Pantalon cuire</p>
+                <p class="price">100.000 Ar</p>
+            </div>
+        </div>
+        <div class="form-outline d-flex" style="width: 90px; height: 40px;">
+            <button class="decrement btn btn-primary mx-1">-</button>
+            <input type="number" class="quantity form-control" style="width: 40px; height: 40px;" />
+            <button class="increment btn btn-primary mx-1">+</button>
+        </div>
+    </div>
+</div>
