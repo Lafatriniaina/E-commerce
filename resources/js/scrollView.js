7d50e15876ddc4da583b69fe3ce609e1bf15582f
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-11-21 23:46:40 +0300

    ScrollView for products

--- resources/js/scrollView.js
+++ resources/js/scrollView.js
@@ -2,24 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var leftArrow = document.getElementById('leftArrow');
     var rightArrow = document.getElementById('rightArrow');
    
-    var products = document.querySelectorAll('.border.border-1.rounded-3.border-grey');
-   
-    products.forEach(function(product, index) {
-        product.addEventListener('mouseover', function() {
-            if(index === 0) {
-                rightArrow.style.display = 'block';
-            }
-            else if(index === products.length - 1) {
-                leftArrow.style.display = 'block';
-            }
-        });
-    
-        product.addEventListener('mouseout', function() {
-            leftArrow.style.display = 'none';
-            rightArrow.style.display = 'none';
-        });
-    });
-   
     var scrollContainer = document.querySelector('.d-flex.overflow-x-auto');
    
     rightArrow.addEventListener('click', function() {
@@ -27,13 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
         var maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
             if(scrollAmount < maxScroll) {
                 scrollContainer.scrollTo({
-                    left: scrollAmount + 200,
-                    behavior: 'smooth'
+                   left: scrollAmount + 200,
+                   behavior: 'smooth'
                 });
             }
-        });
-   });
-   
-  
-   
-  
+    });
+ 
+    leftArrow.addEventListener('click', function() {
+        var scrollAmount = scrollContainer.scrollLeft;
+        if(scrollAmount > 0) {
+            scrollContainer.scrollTo({
+                left: scrollAmount - 200,
+                behavior: 'smooth'
+            });
+        }
+    });
+ });
+ 
