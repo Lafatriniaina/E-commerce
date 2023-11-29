050f574941d36090507d7841ed762db72474dfcf
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-11-29 21:23:44 +0300

    Logic for product description

--- app/Http/Controllers/DescriptionController.php
+++ app/Http/Controllers/DescriptionController.php
@@ -2,7 +2,35 @@
 
 namespace App\Http\Controllers;
 
-class DescriptionController
-{
+use App\Models\Products;
+use Illuminate\Http\Request;
 
+class DescriptionController extends Controller
+{
+    public function someMethod(Request $request)
+    {
+        $name = $request->route('name');
+        $categories = $request->route('categories');
+        $prices = $request->route('prices');
+        $quantities = $request->route('quantities');
+        $contents = $request->route('contents');
+        // $image = $request->route('image');
+
+        $product = Products::where('name', $name)
+            ->where('categories', $categories)
+            ->where('prices', $prices)
+            ->where('quantities', $quantities)
+            ->where('contents', $contents)
+            // ->where('image', $image)
+            ->first();
+
+        $contentsArray = explode(' ', $product->contents);
+        $contentsLines = array_chunk($contentsArray, 3);
+        foreach ($contentsLines as $key => $line) {
+            $contentsLines[$key] = implode(' ', $line);
+        }
+        $product->contents = implode('<br>', $contentsLines);
+
+        return view('layouts.Description', ['product' => $product]);
+    }
 }
