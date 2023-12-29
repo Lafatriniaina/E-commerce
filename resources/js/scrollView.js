b1c3d4eba67e691f8890ab20e5fb0b7188b809bc
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-12-29 22:05:45 +0300

    scrollView

--- resources/js/scrollView.js
+++ resources/js/scrollView.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var leftArrow = document.getElementById('leftArrow');
-    var rightArrow = document.getElementById('rightArrow');
-   
-    var scrollContainer = document.querySelector('.d-flex.overflow-x-auto');
-   
-    rightArrow.addEventListener('click', function() {
-        var scrollAmount = scrollContainer.scrollLeft;
-        var maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+    var leftArrows = document.querySelectorAll('#leftArrow');
+    var rightArrows = document.querySelectorAll('#rightArrow');
+    var scrollContainers = document.querySelectorAll('.d-flex.overflow-x-auto');
+ 
+    for(let i = 0; i < scrollContainers.length; i++){
+        let leftArrow = leftArrows[i];
+        let rightArrow = rightArrows[i];
+        let scrollContainer = scrollContainers[i];
+        
+        rightArrow.addEventListener('click', function() {
+            var scrollAmount = scrollContainer.scrollLeft;
+            var maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
             if(scrollAmount < maxScroll) {
                 scrollContainer.scrollTo({
                    left: scrollAmount + 200,
                    behavior: 'smooth'
                 });
             }
-    });
+        });
  
-    leftArrow.addEventListener('click', function() {
-        var scrollAmount = scrollContainer.scrollLeft;
-        if(scrollAmount > 0) {
-            scrollContainer.scrollTo({
-                left: scrollAmount - 200,
-                behavior: 'smooth'
-            });
-        }
-    });
+        leftArrow.addEventListener('click', function() {
+            var scrollAmount = scrollContainer.scrollLeft;
+            if(scrollAmount > 0) {
+                scrollContainer.scrollTo({
+                   left: scrollAmount - 200,
+                   behavior: 'smooth'
+                });
+            }
+        });
+    }
  });
  
