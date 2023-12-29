d880a74be79cca6ead31c33769b5cbe373820b43
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-12-29 22:08:22 +0300

    Necessary routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\DescriptionController;
 use \App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductsFiltered;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,15 @@ use \App\Http\Controllers\CartController;
 |
 */
 
-Route::get('/', [ProductsController::class, 'index'])->name('home');
-Route::post('/', [ProductsController::class, 'addToCart'])->name('home');
-// Route::delete('/{id}', [CartController::class, 'removeFromCart'])->name('remove');
+Route::prefix('/')->group(function() {
+    Route::get('/', [ProductsController::class, 'index'])->name('home');
+    Route::post('/', [CartController::class, 'addToCart'])->name('home');
+});
+
+Route::prefix('/cart')->group(function() {
+    Route::get('/count', 'CartController@count')->name('cart.count');
+    Route::delete('/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
+});
 
 Route::prefix('/user')->group(function () {
     Route::post('/register', [RegisteredUserController::class, 'store'])->name('auth.register');
