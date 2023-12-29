776838086e9523c80620ca0b1a5aa796e6149215
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-12-29 22:07:09 +0300

    Modify hidden form

--- resources/views/layouts/HiddenForm/HiddenForm.blade.php
+++ resources/views/layouts/HiddenForm/HiddenForm.blade.php
@@ -1,9 +1,9 @@
-<form action="{{ route('home') }}" method="POST">
+<form action="{{ route('home') }}" method="POST" class="form">
     @csrf
     <input type="hidden" value="{{ $product->id }}" name="id">
     <input type="hidden" value="{{ $product->name }}" name="name">
     <input type="hidden" value="{{ $product->price }}" name="price">
     <input type="hidden" value="{{ $product->image }}" name="image">
     <input type="hidden" value="1" name="quantity">
-    <button type="submit" class="btn btn-primary">Ajouter au panier</button>
-  </form>
+    <button type="submit" class="btn btn-primary add-to-cart">Ajouter au panier</button>
+</form>
